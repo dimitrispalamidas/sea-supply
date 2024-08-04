dffcfe80a6cee42292352988efd81c81d726dbc0
Dimitris Palamidas <palamidas.dim@gmail.com>
2024-08-04 14:36:50 +0300

    fix bug on sidebar v.2

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      Authorization Page - Coming Soon{" "}
-      <Link
-        href='/order'
-        className='text-white bg-blue-500 hover:bg-blue-700 px-4 py-2 rounded'
-      >
-        Go to Order Instead➡️
-      </Link>
-    </div>
+    <>
+      <div className='mb-5'>Authorization Page - Coming Soon</div>
+      <div>
+        <Link
+          href='/order'
+          className='text-white bg-blue-500 hover:bg-blue-700 px-4 py-2 rounded'
+        >
+          Go to Order Instead➡️
+        </Link>
+      </div>
+    </>
   );
 }
