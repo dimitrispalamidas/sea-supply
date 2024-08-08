9b8141861f22e7e0b84c5c9fe5c2e0b8896ed1dc
Dimitris Palamidas <palamidas.dim@gmail.com>
2024-08-09 01:26:48 +0300

    Fix bugs Images for subcategories 3.0

--- app/(main)/order/page.tsx
+++ app/(main)/order/page.tsx
@@ -80,27 +80,29 @@ const PersonalItemsSection: React.FC<SectionProps> = ({
       <h2 className='text-2xl font-bold mb-4 flex justify-center text-gray-700'>
         Personal Items
       </h2>
-      <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         {categories[0]?.subcategories?.map((sub) => (
-          <Button
-            key={sub.name}
-            variant={"primary"}
-            onClick={() => {
-              setSelectedCategory("Personal");
-              setSelectedSubcategory(sub.name);
-              setSelectedSubSubcategory(null);
-            }}
-            className='flex justify-between items-center'
-          >
-            {sub.name}
-            <Image
-              width={48}
-              height={48}
-              src={sub.imgSrc ? sub.imgSrc : "/no-image.jpg"}
-              alt={sub.name}
-              className='rounded'
-            />
-          </Button>
+          <div key={sub.name} className='flex justify-center'>
+            <Button
+              variant={"primary"}
+              size={"lg"}
+              onClick={() => {
+                setSelectedCategory("Personal");
+                setSelectedSubcategory(sub.name);
+                setSelectedSubSubcategory(null);
+              }}
+              className='flex justify-between items-center max-w-xs w-full'
+            >
+              {sub.name}
+              <Image
+                width={48}
+                height={48}
+                src={sub.imgSrc ? sub.imgSrc : "/no-image.jpg"}
+                alt={sub.name}
+                className='rounded'
+              />
+            </Button>
+          </div>
         ))}
       </div>
     </div>
@@ -117,27 +119,29 @@ const ProfessionalItemsSection: React.FC<SectionProps> = ({
       <h2 className='text-2xl font-bold mb-4 flex justify-center text-gray-700'>
         Professional Items
       </h2>
-      <div className='grid grid-cols-2 lg:grid-cols-3 gap-4'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         {categories[1]?.subcategories?.map((sub) => (
-          <Button
-            key={sub.name}
-            variant={"primary"}
-            onClick={() => {
-              setSelectedCategory("Personal");
-              setSelectedSubcategory(sub.name);
-              setSelectedSubSubcategory(null);
-            }}
-            className='flex justify-between items-center'
-          >
-            {sub.name}
-            <Image
-              width={48}
-              height={48}
-              src={sub.imgSrc ? sub.imgSrc : "/no-image.jpg"}
-              alt={sub.name}
-              className='rounded'
-            />
-          </Button>
+          <div key={sub.name} className='flex justify-center'>
+            <Button
+              variant={"primary"}
+              size={"lg"}
+              onClick={() => {
+                setSelectedCategory("Personal");
+                setSelectedSubcategory(sub.name);
+                setSelectedSubSubcategory(null);
+              }}
+              className='flex justify-between items-center max-w-xs w-full'
+            >
+              {sub.name}
+              <Image
+                width={48}
+                height={48}
+                src={sub.imgSrc ? sub.imgSrc : "/no-image.jpg"}
+                alt={sub.name}
+                className='rounded'
+              />
+            </Button>
+          </div>
         ))}
       </div>
     </div>
